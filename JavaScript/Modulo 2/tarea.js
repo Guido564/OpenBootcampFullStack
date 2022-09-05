6b342760cda7bcdb889188d31d7266c899c0beae
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-05 19:20:28 -0300

    Tarea 2 JS

---
+++ JavaScript/Modulo 2/tarea.js
@@ -0,0 +1,12 @@
+let libroFav = {
+    titulo: 'La metamorfosis',
+    autor: 'Franz Kafka',
+    fecha: 1912,
+    url: 'Google',
+}
+
+let fechaDeNacimiento = new Date(1997, 5, 19)
+
+let misDatos = ['Guido', 25, true, fechaDeNacimiento, libroFav];
+
+console.log(misDatos)
