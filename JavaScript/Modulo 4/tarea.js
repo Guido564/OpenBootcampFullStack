245ff3bebd53d00b0b479fd6bb93e8fcbecb0455
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-05 22:26:48 -0300

    Actividades modulo 4

---
+++ JavaScript/Modulo 4/tarea.js
@@ -0,0 +1,26 @@
+let nombre = 'Guido'
+let apellido = 'Pruzzo'
+
+let estudiante = `   ${nombre} ${apellido}   `
+console.log(estudiante)
+
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+
+let estudianteMinus = estudianteMayus.toLowerCase()
+console.log(estudianteMinus)
+
+let contadorCadena = estudiante.length
+console.log(contadorCadena)
+
+let primeraLetraNombre = nombre[0]
+console.log(primeraLetraNombre)
+
+let primeraLetraApellido = apellido[0]
+console.log(primeraLetraApellido)
+
+let estudianteSinEspacios = estudiante.trim()
+console.log(estudianteSinEspacios)
+
+let encontrarNombre = estudiante.includes(nombre)
+console.log(encontrarNombre)
