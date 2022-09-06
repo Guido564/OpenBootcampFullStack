8cce46eb236de52e112a8352fec7d73e8a40f70b
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-06 08:43:54 -0300

    Modulo 5

---
+++ JavaScript/Modulo 5/tarea.js
@@ -0,0 +1,10 @@
+let altura = 182;
+let alturaFloat = 1.82;
+let peso = 99.4;
+let alturaRedondeada = Math.ceil(alturaFloat);
+console.log(alturaRedondeada);
+let pesoRedondeado = Math.round(peso);
+console.log(pesoRedondeado);
+
+let maximosValoresExactitud = 1.7976931348623157e+308 === 1.7976931348623157e+308 + 1
+console.log(maximosValoresExactitud)
