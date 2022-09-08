8a4fe8aa94fb95573e220b485c94be92a9f2a02d
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-08 09:52:47 -0300

    Terminado modulo 10

---
+++ JavaScript/Modulo 10.1/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import {multiplica, suma} from "./modulos/aritmetica.js";
+
+console.log(suma(2,2))
+
+console.log(chalk.green(multiplica(3,3)));
---
+++ JavaScript/Modulo 10/index.js
@@ -0,0 +1,5 @@
+const { suma, multiplica } = require("./controller");
+
+
+console.log(suma(2, 2));
+console.log(multiplica(3, 3));
